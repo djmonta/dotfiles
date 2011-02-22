4a98defa537acdf270869c925b609f9d02e643af
monta <djmonta@gmail.com>
2011-02-22 16:38:27 +0000

    user.name djmonta

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; Last Modified: 2011/02/23-01:09:29
+;; Last Modified: 2011/02/23-01:37:39
 
 ;; ~/.emacs.d をロードパスに追加
 ;(let ((default-directory "~/.emacs.d"))
@@ -50,7 +50,7 @@
 (load "init-shell-pop")
 (load "init-twitter")
 
-;; Platform-dependent OS別ファイル読み込み
+;; Platform-dependent
 (setq os-init-file
       (cond ((eq window-system 'mac) "carbon-mac-init.el")
 			((eq window-system 'ns) "cocoa-mac-init.el")

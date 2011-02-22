390b52e1e514af40ca2c600ac42a6b9cbe7d7d46
monta <djmonta@gmail.com>
2011-02-22 19:36:05 +0000

    git-emacs と git.elを使う

--- .emacs.d/conf/init-git.el
+++ .emacs.d/conf/init-git.el
@@ -1,7 +1,5 @@
-(require 'egg)
 (add-to-list 'exec-path "/usr/local/bin")
 
-<<<<<<< HEAD
 (require 'git-emacs)
 (fmakunbound 'git-status)   ; Possibly remove Debian's autoloaded version
 (require 'git-emacs-autoloads)
@@ -11,5 +9,3 @@
   "Minor mode for incremental blame for Git." t)
 
 (require 'git)
-=======
->>>>>>> 24823d18beef6d2c6496e40e4f7f6c82926c48a9

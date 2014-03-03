380ed3dadad8e4b2927994ca00792ddcc65c7af6
monta <djmonta@gmail.com>
2014-03-04 06:15:35 +0900

    add submodule zsh-notify

---
+++ .zsh/plugins/zsh-notify.zsh
@@ -0,0 +1,9 @@
+#
+# zsh-notify
+#
+#  https://github.com/marzocchi/zsh-notify
+#
+[ -f ${HOME}/.zsh/zsh-notify/zsh-notify.zsh ] \
+    || { echo '...skip'; return; }
+
+source ${HOME}/.zsh/zsh-notify/zsh-notify.zsh

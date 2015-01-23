363c837e3b31979406fc34be6af36ae4e5699d5a
monta <djmonta@gmail.com>
2015-01-23 21:59:18 +0900

    一応動く

--- etc/init/osx/50-fonts.sh
+++ etc/init/osx/50-fonts.sh
@@ -4,14 +4,6 @@ trap 'echo Error: $0: stopped' ERR
 set -e
 set -u
 
-
-# Copy fonts
-{
-  pushd $DOTFILES/etc/lib/fonts/; setdiffA=(*); popd
-  pushd ~/Library/Fonts/; setdiffB=(*); popd
-  setdiff
-} >/dev/null
-
 #
 # Testing the judgement system
 # {{{
@@ -19,10 +11,8 @@ if [[ -n ${DEBUG:-} ]]; then echo "$0" && exit 0; fi
 #}}}
 
 
-if (( ${#setdiffC[@]} > 0 )); then
-  e_header "Copying fonts (${#setdiffC[@]})"
-  for f in "${setdiffC[@]}"; do
-    #e_arrow "$f"
-    cp "$DOTFILES/etc/lib/fonts/$f" ~/Library/Fonts/
-  done
-fi
+printf "Copying fonts ..."
+for f in $DOTFILES/etc/lib/fonts/*; do
+  cp "$f" ~/Library/Fonts/
+done
+printf "done!\n"

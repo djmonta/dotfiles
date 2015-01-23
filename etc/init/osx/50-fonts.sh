2ffc59dbb7b0619086f57e0627537823d3f39668
monta <djmonta@gmail.com>
2015-01-23 21:42:59 +0900

    まだ動くか分からない

--- etc/init/osx/50-fonts.sh
+++ etc/init/osx/50-fonts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+trap 'echo Error: $0: stopped' ERR
+set -e
+set -u
+
+
 # Copy fonts
 {
   pushd $DOTFILES/etc/lib/fonts/; setdiffA=(*); popd
@@ -7,10 +12,17 @@
   setdiff
 } >/dev/null
 
+#
+# Testing the judgement system
+# {{{
+if [[ -n ${DEBUG:-} ]]; then echo "$0" && exit 0; fi
+#}}}
+
+
 if (( ${#setdiffC[@]} > 0 )); then
   e_header "Copying fonts (${#setdiffC[@]})"
   for f in "${setdiffC[@]}"; do
-    e_arrow "$f"
-    cp "$DOTFILES/conf/osx/fonts/$f" ~/Library/Fonts/
+    #e_arrow "$f"
+    cp "$DOTFILES/etc/lib/fonts/$f" ~/Library/Fonts/
   done
 fi

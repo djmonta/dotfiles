25f5dd0a58d113e87c965a0a8b6a8b5f2238c379
monta <djmonta@gmail.com>
2015-01-23 21:18:07 +0900

    これ動くか不明

---
+++ etc/init/osx/50-fonts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copy fonts
+{
+  pushd $DOTFILES/conf/osx/fonts/; setdiffA=(*); popd
+  pushd ~/Library/Fonts/; setdiffB=(*); popd
+  setdiff
+} >/dev/null
+
+if (( ${#setdiffC[@]} > 0 )); then
+  e_header "Copying fonts (${#setdiffC[@]})"
+  for f in "${setdiffC[@]}"; do
+    e_arrow "$f"
+    cp "$DOTFILES/conf/osx/fonts/$f" ~/Library/Fonts/
+  done
+fi

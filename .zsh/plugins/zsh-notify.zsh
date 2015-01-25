e35bbc2bade1710698bc4fe88cdecd78142b98f9
monta <djmonta@gmail.com>
2015-01-25 14:04:24 +0900

    なんか消えてた

---
+++ .zsh/plugins/zsh-notify.zsh
@@ -0,0 +1,9 @@
+#
+# zsh-notify
+#
+#  https://github.com/marzocchi/zsh-notify
+#
+[ -f ${HOME}/.zsh/zsh-notify/notify.plugin.zsh ] \
+    || { echo '...skip'; return; }
+
+source ${HOME}/.zsh/zsh-notify/notify.plugin.zsh

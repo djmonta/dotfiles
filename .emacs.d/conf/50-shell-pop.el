ca27a916155d7343a7f3a127d21ac267fe1c0c99
monta <djmonta@gmail.com>
2015-01-29 15:44:38 +0900

    :hammer: Modify: conf/50-shell-pop.el
    - エラーが出るところをコメントアウト
    - Hotkey を f8 -> f9 に変更

--- .emacs.d/conf/50-shell-pop.el
+++ .emacs.d/conf/50-shell-pop.el
@@ -1,6 +1,6 @@
 ; shell-pop の設定
 ; http://d.hatena.ne.jp/kyagi/20090601/1243841415
 (require 'shell-pop)
-(global-set-key [f8] 'shell-pop)
-(shell-pop-set-internal-mode "ansi-term")
+(global-set-key [f9] 'shell-pop)
+;(shell-pop-set-internal-mode "ansi-term")
 (shell-pop-set-window-height 40)

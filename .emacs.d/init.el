a8daf62d8f3673106fb53e1a400316a00ac2d3de
monta <djmonta@gmail.com>
2015-01-29 15:21:14 +0900

    :hammer: Modify: Migrate manual installed elisp to package.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; Last Modified: 2013/09/21-10:39:25
+;; Last Modified: 2015/01/29-14:57:17
 
 (unless (boundp 'user-emacs-directory)
   (defvar user-emacs-directory (expand-file-name "~/.emacs.d/")))
@@ -9,6 +9,37 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-refresh-contents)
 (package-initialize)
+
+;; 自動インストール設定
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書く。
+    auto-async-byte-compile
+    auto-install
+    auto-save-buffers-enhanced
+    clmemo
+    e2wm
+    git-commit-mode
+    git-rebase-mode
+    init-loader
+    magit
+    open-junk-file
+    paredit
+    shell-pop
+    twittering-mode
+    window-layout
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
+
 
 ;; init-loader
 (require 'init-loader)

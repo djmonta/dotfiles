c7e30787bbc69753f851741ced13495d4439dd61
monta <djmonta@gmail.com>
2015-01-29 15:20:30 +0900

    :new: Add: Migrate manual installed elisp to package.el

---
+++ .emacs.d/elpa/clmemo-20140321.715/clmemo-pkg.el
@@ -0,0 +1,5 @@
+(define-package "clmemo" "20140321.715" "Change Log MEMO" 'nil :url "https://github.com/ataka/clmemo" :keywords
+  '("convenience"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

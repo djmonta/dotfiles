89bd21f5be68f23fbf1f9b3cb937ca89f4da01e4
monta <djmonta@gmail.com>
2015-03-12 16:18:50 +0900

    :up: modify: sudo,,,

--- etc/init/debian/install-packages.sh
+++ etc/init/debian/install-packages.sh
@@ -30,20 +30,19 @@ if [[ "$REPLY" =~ ^[Yy]$ ]]; then
         sleep 60;
         kill -0 "$$" || exit
     done 2>/dev/null &
-    
+
     echo 'apt-get update...'
-    apt-get update
+    sudo apt-get update
 
     echo 'apt-get upgrade...'
-    apt-get -y upgrade
-
+    sudo apt-get -y upgrade
 
     for package in "${PACKAGES[@]}"
     do
         if dpkg -l | grep -q "^${package}"; then
             echo "Skip: apt-get install ${package}"
         else
-            apt-get -y install ${package}
+            sudo apt-get -y install ${package}
         fi
     done
 

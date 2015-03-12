210b1133c0c1cedba7a50526649d9e40d5a16b0c
monta <djmonta@gmail.com>
2015-03-12 13:22:56 +0900

    :new: add: percol-tmux-select-pane

---
+++ .zsh/functions/percol-tmux.zsh
@@ -0,0 +1,11 @@
+# uu59のメモ | percolでtmuxのペインを選択するやつ
+# http://blog.uu59.org/2013-10-25-percol-tmux-select-pane.html
+
+function percol-tmux-select-pane() {
+  local selected
+  selected=$(tmux list-panes -s -F '#{window_index} #{pane_index} #{pane_title}' | percol)
+  local window=$(echo $selected | cut -f1 -d " ")
+  local pane=$(echo $selected | cut -f2 -d " ")
+  tmux select-window -t $window
+  tmux select-pane -t $pane
+}

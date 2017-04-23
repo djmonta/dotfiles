4c837b3bd5a3341a29688a0963994f99a26ac70a
monta <djmonta@gmail.com>
2017-04-23 11:25:12 +0900

    update

--- etc/init/osx/app/karabiner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
-
-$cli set remap.jis_commandL2commandL_eisuu 1
-/bin/echo -n .
-$cli set remap.jis_commandR2commandR_kana 1
-/bin/echo -n .
-/bin/echo

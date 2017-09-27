18c2a1e6671d7837524c9bca4ee00de9abb0d24b
Sachiko Miyamoto <djmonta@gmail.com>
2017-09-27 15:44:37 +0900

    Test pullrequest (#1)
    * :up: fix nodenv version.
    
    * fix indent.

--- etc/init/osx/40-brewfileinstall.sh
+++ etc/init/osx/40-brewfileinstall.sh
@@ -8,17 +8,17 @@ if brew commands -1 | grep -q "^file"; then
 	echo -n 'Install Homebrew/Cask/MAS Application now? (y/N) '
 	read
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-	  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+	  	export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-	  if hostname | grep -q "Mac-mini\.local$" ; then
-	  	export HOMEBREW_BREWFILE=${HOME}/.brewfile/Brewfile
+	  	if hostname | grep -q "Mac-mini\.local$" ; then
+	  		export HOMEBREW_BREWFILE=${HOME}/.brewfile/Brewfile
 		else
-	  	export HOMEBREW_BREWFILE=${HOME}/.brewfile/Brewfile.MBA
+	  		export HOMEBREW_BREWFILE=${HOME}/.brewfile/Brewfile.MBA
 		fi
 
-	  brew file install
+		brew file install
 	fi
 else
 	echo 'Requirement: brew-file' 1>&2
-  exit 1
+exit 1
 fi

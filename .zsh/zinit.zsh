fa280594c93aee8de146e345adab7ea8658176f6
monta <djmonta@gmail.com>
2022-11-12 09:18:49 +0900

    fix zinit

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -1,9 +1,10 @@
 
 ### Added by Zinit's installer
+### Added by Zinit's installer
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
     command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.zinit/bin" && \
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
         print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
@@ -20,7 +21,7 @@ zinit light "zsh-users/zsh-completions"
 zinit light "zsh-users/zsh-autosuggestions"
 
 zinit ice wait"!0" lucid atinit"zpcompinit; zpcdreplay"
-zinit light "zdharma/fast-syntax-highlighting"
+zinit light "zdharma-continuum/fast-syntax-highlighting"
 
 zinit light "mollifier/anyframe"
 

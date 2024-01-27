0723fe09bbc855625810302604ce884a15704d60
monta <monta@ednity.com>
2024-01-27 15:35:27 +0900

    update some setting

--- .zsh/.fzf.zsh
+++ .zsh/.fzf.zsh
@@ -1,15 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$USR_LOCAL/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$USR_LOCAL/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$USR_LOCAL/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-if [ -f $USR_LOCAL/opt/fzf/shell/key-bindings.zsh ] ; then
-  source "$USR_LOCAL/opt/fzf/shell/key-bindings.zsh"
-fi
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"

fd9e518a696ede4a69977fa911c07296bb7145be
monta <djmonta@gmail.com>
2025-03-06 08:16:10 +0900

    add ghostty config

--- .config/zsh/utils.zsh
+++ .config/zsh/utils.zsh
@@ -112,12 +112,12 @@ function do_enter() {
     echo
     # ls　↓おすすめ
     ls_abbrev
-    if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
-        echo
-        echo -e "\e[0;33m--- git status ---\e[0m"
-        git status -sb
-    fi
-    zle reset-prompt
+    # if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
+    #     echo
+    #     echo -e "\e[0;33m--- git status ---\e[0m"
+    #     git status -sb
+    # fi
+    # zle reset-prompt
     return 0
 }
 

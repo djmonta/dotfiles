57a6e0ba022863f29e5456fe2344dcf4007432bc
monta <djmonta@gmail.com>
2026-08-12 19:37:19 +0900

    Set OS X defaults

--- darwin.nix
+++ darwin.nix
@@ -2,6 +2,9 @@
   config,
   ...
 }:
+let
+  home = config.users.users.monta.home;
+in
 {
   # Determinate Nix owns the daemon and /etc/nix. Do not let nix-darwin take over.
   nix.enable = false;
@@ -11,7 +14,7 @@
 
   users.users.monta.home = "/Users/monta";
 
-  environment.darwinConfig = "${config.users.users.monta.home}/dotfiles/flake.nix";
+  environment.darwinConfig = "${home}/dotfiles/flake.nix";
 
   # Needed so /etc/zshrc loads nix-darwin environment.
   programs.zsh.enable = true;
@@ -24,6 +27,8 @@
       autohide-delay = 0.2;
       autohide-time-modifier = 0.2;
       magnification = true;
+      persistent-apps = [];
+      showhidden = true;
       tilesize = 55;
       wvous-tr-corner = 4; # Desktop
       wvous-bl-corner = 2; # Mission Control
@@ -31,10 +36,43 @@
     };
     finder = {
       AppleShowAllFiles = true;
+      NewWindowTarget = "Home";
+      ShowExternalHardDrivesOnDesktop = false;
+      ShowMountedServersOnDesktop = false;
+      ShowRemovableMediaOnDesktop = false;
       ShowPathbar = true;
       ShowStatusBar = true;
     };
-    trackpad.Clicking = true;
+    screencapture.type = "png";
+    NSGlobalDomain.AppleShowScrollBars = "WhenScrolling";
     NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+    trackpad.Clicking = true;
+    trackpad.TrackpadCornerSecondaryClick = 2;
+    NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+    NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
+    NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
+    NSGlobalDomain.PMPrintingExpandedStateForPrint = true;
+    CustomUserPreferences = {
+      "com.apple.Safari" = {
+        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" = true;
+        "com.apple.Safari.IncludeDevelopMenu" = true;
+        "com.apple.Safari.WebKitDeveloperExtrasEnabledPreferenceKey" = true;
+        "com.apple.Safari.IncludeInternalDebugMenu" = true;
+        "com.apple.Safari.ShowFullURLInSmartSearchField" = true;
+        "com.apple.Safari.ShowStatusBar" = true;
+        "com.apple.Safari.AutoFillPasswords" = false;
+      };
+    };
   };
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
+
+  # Not expressible via system.defaults (filesystem flag, not a plist).
+  system.activationScripts.postActivation.text = ''
+    if [ -d ${home}/Library ]; then
+      chflags nohidden ${home}/Library
+    fi
+  '';
 }
